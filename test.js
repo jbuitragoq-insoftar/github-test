0f7dda442031a3c8ecf9abbd001d6ddcea06f176
John Freddy Buitrago Quevedo <john.buitrago@insoftar.com>
2023-01-20 14:54:24 -0500

    feat: changes in test.js

--- test.js
+++ test.js
@@ -2,3 +2,7 @@
 console.log('file test: stage');
 console.log('this is a new change');
 console.log('add line 4');
+console.log('add line 5');
+console.log('add line 6');
+console.log('add line 7');
+console.log('developer: Johnf');
